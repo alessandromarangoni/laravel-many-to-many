4a47f5e90212b2805743da60a2bbfccb71af4472
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-07-19 15:30:53 +0200

    feat add controller and route show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,11 +9,32 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $project=portfolio::with('type','tecnologies')->get();
+        $projects = portfolio::with('type','tecnologies')->get();
+        
+        return(response()->json($response =[
+            'success'=>true,
+            'results'=>$projects,
+        ]));
+    }
+
+    public function show($id){
+        $project = portfolio::find($id);
         
         return(response()->json($response =[
             'success'=>true,
             'results'=>$project,
         ]));
     }
+
+    // public function show($id){
+    //     $project = portfolio::with('type','tecnologies')->find($id);
+        
+    //     $response = [
+    //         "success" => true,
+    //         "results" => $project
+    //     ];
+
+    //     return response()->json($response);
+    // }
+
 }
